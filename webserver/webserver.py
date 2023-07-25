09940995c02e41b7945f97703e84245750340743
Philipp von Neffe <philipp.von-neffe@tum.de>
2023-07-25 16:30:43 +0200

    threadedhttp server for google chrome

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -8,14 +8,16 @@ directory = "serve/"
 # Set the port number for the server
 port = 8000
 
-# Create a handler to serve the files
-handler = http.server.SimpleHTTPRequestHandler
-
 # Change to the specified directory
 os.chdir(directory)
 
+# Create a ThreadingHTTPServer to serve the files
+class ThreadedHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
 # Start the server
-with socketserver.TCPServer(("", port), handler) as httpd:
+with socketserver.ThreadingTCPServer(("", port), ThreadedHTTPRequestHandler) as httpd:
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
